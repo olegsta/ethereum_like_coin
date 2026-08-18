5e57388734803e5eb52af98d68acd7858e6b3b17
oleh starosvitskyi <oleg.starosvitskij@gmail.com>
2026-08-18 18:13:46 +0200

    fix

--- app/chains/arbitrum.py
+++ app/chains/arbitrum.py
@@ -5,8 +5,8 @@ CHAIN_NAME = "arbitrum"
 
 DB_NAME = "arbitrum-shkeeper"
 # testnet
-# FULLNODE_URL = 'https://sepolia-rollup.arbitrum.io/rpc'
-FULLNODE_URL = "http://arbitrum:8545"
+FULLNODE_URL = 'https://sepolia-rollup.arbitrum.io/rpc'
+# FULLNODE_URL = "http://arbitrum:8545"
 
 ENV = {
     "network": (
